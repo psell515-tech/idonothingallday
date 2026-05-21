0ff6310906afb8f1b80ed1d8fa48f84ccbdd3edb
psell515-tech <psell515@gmail.com>
2026-05-21 10:43:20 -0400

    Update contact.js

---
+++ idnadhome/functions/contact.js
@@ -0,0 +1,14 @@
+export async function onRequestPost(context) {
+  const data = await context.request.json();
+
+  const fileName = `${Date.now()}-${data.email}.json`;
+
+  await context.env.MY_BUCKET_IDNAD_CONTACT.put(
+    fileName,
+    JSON.stringify(data, null, 2)
+  );
+
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
